f0887e807d00325ccb849f3f5a12dbe4c476779c
unknown <fishermujahid@gmail.com>
2024-02-23 09:11:16 +0200

    finished

--- Api/index.js
+++ Api/index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000;
 require("./models/route");
 require("./config/connectDB");
 const User = require("./config/connectDB");
+const { ObjectId } = require("mongodb");
 
 app.use(express.static("./static"), express.json());
 
@@ -11,15 +12,38 @@ app.listen(PORT, () => {
   console.log(`The server is running on port : ${PORT} 👌`);
 });
 
-User.find()
-  .then((users) => {
+//All Users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
     console.log("All users:", users);
-  })
-  .catch((error) => {
+  } catch (error) {
     console.error("Error fetching users:", error);
-  });
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
-app.post("/users", async (req, res) => {
+
+//Single User
+app.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+  if (ObjectId.isValid(req.params.id)) {
+      User.findOne({ _id: new ObjectId(id) })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could'nt fetch data." });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid data id" });
+  }
+});
+
+
+//Register a user
+app.post("/register", async (req, res) => {
   try {
     const { firstName, lastName } = req.body;
 
@@ -38,3 +62,21 @@ app.post("/users", async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 });
+
+
+//Delete a user
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  if (ObjectId.isValid(req.params.id)) {
+      User.deleteOne({ _id: new ObjectId(id) })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could'nt fetch data." });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid data id" });
+  }
+});
+
