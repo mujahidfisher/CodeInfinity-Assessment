fec85d2ce0aff3118a455142d14b13e69a53563b
unknown <fishermujahid@gmail.com>
2024-02-27 10:56:05 +0200

    done

--- Api/config/connectDB.js
+++ Api/config/connectDB.js
@@ -1,18 +1,29 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {
-      firstName: {
-        type: String,
-        required: true
-      },
-      lastName: {
-        type: String,
-        required: true
-      }
-    }
-  })
-  
-  const User = mongoose.model('User', userSchema);
-  
-  module.exports = User;
+  name: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  idNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  dob: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.index({ idNumber: 1, dob: 1 }, { unique: true });
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- Api/index.js
+++ Api/index.js
@@ -4,7 +4,6 @@ const PORT = process.env.PORT || 3000;
 require("./models/route");
 require("./config/connectDB");
 const User = require("./config/connectDB");
-const { ObjectId } = require("mongodb");
 
 app.use(express.static("./static"), express.json());
 
@@ -12,11 +11,11 @@ app.listen(PORT, () => {
   console.log(`The server is running on port : ${PORT} 👌`);
 });
 
-//All Users
+// Fetch all users
 app.get("/users", async (req, res) => {
   try {
     const users = await User.find();
-    res.status(200).json({users});
+    res.status(200).json({ users });
     console.log("All users:", users);
   } catch (error) {
     console.error("Error fetching users:", error);
@@ -24,57 +23,32 @@ app.get("/users", async (req, res) => {
   }
 });
 
-//Single User
-app.get("/user/:id", (req, res) => {
-  const { id } = req.params;
-  if (ObjectId.isValid(req.params.id)) {
-    User.findOne({ _id: new ObjectId(id) })
-      .maxTimeMS(30000)
-      .then((doc) => {
-        res.status(200).json(doc);
-      })
-      .catch((err) => {
-        res.status(500).json({ error: "Could'nt fetch data." });
-      });
-  } else {
-    res.status(500).json({ error: "Not a valid data id" });
-  }
-});
-
-//Register a user
+// Register a user
 app.post("/register", async (req, res) => {
   try {
-    const { firstName, lastName } = req.body;
+    const { firstName, lastName, idNumber, dob } = req.body;
 
+    const existingUser = await User.findOne({ idNumber });
+    if (existingUser) {
+      return res.status(400).json({ error: "ID number already exists" });
+    }
+
+    // Create a new user document
     const newUser = new User({
       name: {
         firstName,
         lastName,
       },
+      idNumber,
+      dob,
     });
 
+    // Save the new user to the database
     const savedUser = await newUser.save();
 
     res.status(201).json(savedUser);
   } catch (error) {
     console.error("Error creating user:", error);
     res.status(500).json({ error: "Server error" });
-  }
-});
-
-//Delete a user
-app.delete("/delete/:id", (req, res) => {
-  const { id } = req.params;
-  if (ObjectId.isValid(req.params.id)) {
-    User.deleteOne({ _id: new ObjectId(id) })
-      .maxTimeMS(30000)
-      .then((result) => {
-        res.status(200).json(result);
-      })
-      .catch((err) => {
-        res.status(500).json({ error: "Could'nt fetch data." });
-      });
-  } else {
-    res.status(500).json({ error: "Not a valid data id" });
   }
 });
